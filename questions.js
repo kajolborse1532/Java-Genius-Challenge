b1ce848c968f4510b5661d78ff815cf55e245d46
kajol Borse <112473533+kajolborse1532@users.noreply.github.com>
2023-10-06 15:05:38 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,117 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who invented Java Programming?",
+    answer: "James Gosling",
+    options: [
+         "Guido van Rossum",
+         "James Gosling",
+         "Dennis Ritchie",
+         "Bjarne Stroustrup"
+    ]
+  },
+    {
+    numb: 2,
+    question: " Which component is used to compile, debug and execute the java programs?",
+    answer: "JVM",
+    options: [
+        "JRE",
+        "JIT",
+        "JDK",
+        "JVM"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the extension of java code files?",
+    answer: ".java",
+    options: [
+        ".js",
+         ".txt",
+        ".class",
+         ".java"
+    ]
+  },
+    {
+    numb: 4,
+    question: " Which of the following is not an OOPS concept in Java?",
+    answer: "Compilation",
+    options: [
+        "Polymorphism",
+        "Inheritance",
+        "Compilation",
+        "Encapsulation"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is a type of polymorphism in Java Programming?",
+    answer: "Compile time polymorphism",
+    options: [
+        "Multiple polymorphism",
+        "Compile time polymorphism",
+        "Multilevel polymorphism",
+        "Execution time polymorphism"
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "What is Truncation in Java?",
+    answer: "Floating-point value assigned to an integer type",
+    options: [
+      " Integer value assigned to floating type",
+      "Floating-point value assigned to an integer type",
+      "Floating-point value assigned to a Floating type",
+      " Integer value assigned to floating type"
+    ]
+  },
+  {
+    numb: 7,
+    question: " Which exception is thrown when java is out of memory?",
+    answer: "OutOfMemoryError",
+    options: [
+      " MemoryFullException",
+      "MemoryOutOfBoundsException",
+      "OutOfMemoryError",
+      "MemoryError"
+    ]
+},
+
+{
+    numb: 8,
+     question: "Which of these are selection statements in Java?",
+    answer: "if()",
+    options: [
+          " for()          ",
+          "if()          ",
+          " continue",
+          "break"
+        ]
+},
+
+{
+    numb: 9,
+    question: " Which of these keywords is used to define interfaces in Java?",
+    answer: "interface",
+    options: [
+              " Interface",
+              "interface",
+              "Intf",
+              "intf"
+    ]
+},
+{
+    numb: 10,
+    question: "Which of the below is not a Java Profiler?",
+    answer: " JVM",
+    options: [
+      " JVM ",
+      " JConsole",
+      " Eclipse Profiler",
+      " JProfiler"
+    ]
+},       
+        
+];
